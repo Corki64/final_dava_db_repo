47dcd75e593136d831543912b3c5f97bb8d4c42c
Luis Cortez <lac0084@auburn.com>
2020-06-10 20:53:41 -0500

    again

--- src/main/java/edu/au/cc/gallery/tools/UserAdmin/DB.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin/DB.java
@@ -60,7 +60,7 @@ public class DB {
 		PreparedStatement stmt = connection.prepareStatement("select * from users");
 		ResultSet rs = stmt.executeQuery();
 		while (rs.next()) {
-			System.out.println(rs.getString(1) + " | " + rs.getString(2)
+			System.out.println(rs.getString(1) + " | " + rs.getString(2) + " | "
 					+ rs.getString(3));
 		}
 		rs.close();
